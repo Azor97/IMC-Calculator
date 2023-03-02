b61def6b32b4a3ef8a166cfb124f202b160de9a3
Daniel <daniel.pessoa2010@hotmail.com>
2023-03-01 21:25:44 -0300

    Adicionado o style

--- script.js
+++ script.js
@@ -9,31 +9,27 @@ entre 25 e 29,99 acima do peso
 
 
 
-function calcular(){
-    let altura = document.querySelector("#altura")
-    let peso = document.querySelector("#peso")
+function calcular(event) {
+    event.preventDefault();
+    let altura = document.querySelector("#altura").value
+    let peso = document.querySelector("#peso").value
     let total = peso / (altura * altura);
     let resultado = document.querySelector("#resultado")
 
-    switch(total){
-        case total < 17:
-            resultado.innerHTML = `<strong>Seu IMC deu ${total}. Você está muito abaixo do peso!</strong>`
-            break;
-        
-            case total >= 17 || total <= 18.49:
-            resultado.innerHTML = `<strong>Seu IMC deu ${total}. Você está abaixo do peso!</strong>`
-        break; 
-        
-        case total >= 18.5 || total <= 24.99:
-            resultado.innerHTML = `<strong>Seu IMC deu ${total}. Você está com o peso normal!</strong>`
-        break;
-        
-        case total >= 25:
-            resultado.innerHTML = `<strong>Seu IMC deu ${total}. Você está acima do peso!</strong>`
-        break;
-
-        default:
-            resultado.innerHTML = `<strong>Você precisa digitar um valor!</strong>`
+    if(total < 17) {
+        resultado.innerHTML = `Seu IMC é ${total.toFixed(2)}. Você está muito abaixo do peso!`
+    } else if (total >= 17 && total <= 18.49){
+        resultado.innerHTML = `Seu IMC é ${total.toFixed(2)}. Você está abaixo do peso!`
+    } else if (total >= 18.5 && total <= 24.99) {
+        resultado.innerHTML = `Seu IMC é ${total.toFixed(2)}. Você está com o peso normal!`
+    } else if (total >= 25 && total <= 29.99){
+        resultado.innerHTML = `Seu IMC é ${total.toFixed(2)}. Você está acima do peso!`
+    } else if(total >= 30 && total <= 34.99){
+        resultado.innerHTML = `Seu IMC é ${total.toFixed(2)}. Você está obeso!`
+    } else if(total >= 35) {
+        resultado.innerHTML = `Seu IMC é ${total.toFixed(2)}. Você está extremamente obeso!`
+    } else {
+        resultado.innerHTML = `Digite um valor valido!`
     }
 }
 
